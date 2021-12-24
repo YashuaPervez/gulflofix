3275a9d975df06a1039ad55a97a8ba2b8769235b
Yashua Pervez <yashuapervez@gmail.com>
2021-12-24 18:21:15 +0500

    Style home page contact section

--- src/container/Home/Calendar/index.jsx
+++ src/container/Home/Calendar/index.jsx
@@ -10,7 +10,13 @@ const Calendar = () => {
   return (
     <section className="section">
       <Container>
-        <div className="home-calender-section"></div>
+        <div className="home-calender-section">
+          <iframe
+            src="https://meetings.hubspot.com/aadilg?embed=true&parentHubspotUtk=b8da7e28b4bcc48dc0a89dab09bf1932&parentPageUrl=https://www.gulflogix.ae/"
+            className="home-calender-section-iframe"
+            scrolling="no"
+          ></iframe>
+        </div>
       </Container>
     </section>
   );
