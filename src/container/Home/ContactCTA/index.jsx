16c382452e11ea1cc93ae21191cde2ba7d7dca77
Yashua Pervez <yashuapervez@gmail.com>
2021-12-24 18:12:58 +0500

    home business support section styled

--- src/container/Home/ContactCTA/index.jsx
+++ src/container/Home/ContactCTA/index.jsx
@@ -9,12 +9,16 @@ import "./index.scss";
 
 const ContactCTA = () => {
   return (
-    <section className="section">
+    <section className="section home-contact-cta-section">
       <Container>
         <div className="home-contact-cta">
-          <h2>Can’t find what your looking for?</h2>
-          <h3>We’re here to help.</h3>
-          <Link to="/contact">Contact Us</Link>
+          <h2 className="home-contact-cta-heading">
+            Can’t find what your looking for?
+          </h2>
+          <h3 className="home-contact-cta-heading2">We’re here to help.</h3>
+          <Link to="/contact">
+            <span className="underline home-contact-cta-link">Contact Us</span>
+          </Link>
         </div>
       </Container>
     </section>
