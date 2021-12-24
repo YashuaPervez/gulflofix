055fd1a0e91dc6b2b17d69f604d33610ff356106
Yashua Pervez <yashuapervez@gmail.com>
2021-12-24 16:23:16 +0500

    Update feature section design to be pixel perfect

--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -3,8 +3,21 @@ import React from "react";
 //
 import "./index.scss";
 
-const Button = ({ children }) => {
-  return <button className="button">{children}</button>;
+const Button = ({ children, color }) => {
+  let colorClasses = "";
+  switch (color) {
+    case "blue":
+      colorClasses = "button-blue";
+      break;
+    case "orange":
+      colorClasses = "button-orange";
+      break;
+    default:
+      colorClasses = "button-blue";
+      break;
+  }
+
+  return <button className={`button ${colorClasses}`}>{children}</button>;
 };
 
 export default Button;
--- src/container/Home/Features/index.jsx
+++ src/container/Home/Features/index.jsx
@@ -7,28 +7,77 @@ import Button from "../../../components/UI/Button";
 
 //
 import "./index.scss";
+import ClockImage from "../../../images/home-features/clock.png";
+import HandImage from "../../../images/home-features/hand.png";
+import CallImage from "../../../images/home-features/call.png";
+import BulbImage from "../../../images/home-features/bulb.png";
 
 const Features = () => {
   return (
-    <section className="section">
+    <section className="section home-feature">
       <Container>
         <div className="home-features-wrapper">
           <Paper>
             <div className="features">
-              <div className="feature-item">asdasd</div>
-              <div className="feature-item">asdasd</div>
-              <div className="feature-item">asdasd</div>
-              <div className="feature-item">asdasd</div>
+              <div className="feature-item">
+                <div className="feature-item-image">
+                  <img src={ClockImage} />
+                </div>
+                <h3 className="small-heading feature-item-heading">
+                  Efficiency
+                </h3>
+                <p className="sm-text feature-item-desc">
+                  Quick lead times and smart project management to keep project
+                  implementation on track and on time.
+                </p>
+              </div>
+              <div className="feature-item">
+                <div className="feature-item-image">
+                  <img src={HandImage} />
+                </div>
+                <h3 className="small-heading feature-item-heading">
+                  Cost-Effectiveness
+                </h3>
+                <p className="sm-text feature-item-desc">
+                  Cost-effective services and solutions that fit your budget{" "}
+                  {"&"}
+                  zero sweet talk about additional software add-ons.
+                </p>
+              </div>
+              <div className="feature-item">
+                <div className="feature-item-image">
+                  <img src={BulbImage} />
+                </div>
+                <h3 className="small-heading feature-item-heading">
+                  Innovation
+                </h3>
+                <p className="sm-text feature-item-desc">
+                  Keen business analysis out-of-box implementation for rapid
+                  deployment and stress-free implementation.
+                </p>
+              </div>
+              <div className="feature-item">
+                <div className="feature-item-image">
+                  <img src={CallImage} />
+                </div>
+                <h3 className="small-heading feature-item-heading">
+                  Service Excellence
+                </h3>
+                <p className="sm-text feature-item-desc">
+                  Personal, friendly and professional service and straight
+                  forward and unbiased advice and recommendations.
+                </p>
+              </div>
             </div>
           </Paper>
         </div>
         <div className="home-features-cta">
-          <h2 className="cta-heading">
+          <h2 className="black-heading cta-heading">
             We specialise in collaboration, content, application development
             {" & "}
             cloud solutions
           </h2>
-          <Button>Learn more about what we offer</Button>
+          <Button color="orange">Learn more about what we offer</Button>
         </div>
       </Container>
     </section>
