357628e64fa9300d048d5a9f77067211955f7933
Yashua Pervez <yashuapervez@gmail.com>
2022-01-02 20:54:39 +0500

    Contact from connected with hubspot

--- src/components/FormElements/Input.jsx
+++ src/components/FormElements/Input.jsx
@@ -1,30 +1,32 @@
 import React from "react";
+import { useFormContext } from "react-hook-form";
 
 //
 import "./index.scss";
 
-const Input = ({ name, id, placeholder, text }) => {
-  if (text) {
-    return (
-      <div>
+const Input = ({ name, id, placeholder, text, error }) => {
+  const { register } = useFormContext();
+
+  return (
+    <div>
+      {text ? (
         <textarea
           name={name}
           id={id}
           placeholder={placeholder}
           className="form-input form-textarea"
+          {...register(id)}
         ></textarea>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <input
-        name={name}
-        id={id}
-        placeholder={placeholder}
-        className="form-input"
-      />
+      ) : (
+        <input
+          name={name}
+          id={id}
+          placeholder={placeholder}
+          className="form-input"
+          {...register(id)}
+        />
+      )}
+      {error && <span className="form-input-error">{error}</span>}
     </div>
   );
 };
--- src/container/Contact/ContactForm/index.jsx
+++ src/container/Contact/ContactForm/index.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 // Components
 import Container from "../../../components/UI/Container";
@@ -9,9 +12,89 @@ import Button from "../../../components/UI/Button";
 import { Email, Phone, Whatsapp } from "../../../components/icons";
 import "./index.scss";
 
+const contactFormSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  firstname: yup.string().min(3).max(20).required(),
+  phone: yup.number().typeError("Must be a valid number").required(),
+  body: yup.string().max(500).required(),
+});
+
 const ContactForm = ({ data }) => {
-  const submitFormHandler = (e) => {
-    e.preventDefault();
+  const form = useForm({
+    resolver: yupResolver(contactFormSchema),
+  });
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = form;
+
+  const submitFormHandler = async (data) => {
+    // Create the new request
+    var xhr = new XMLHttpRequest();
+    var url =
+      "https://api.hsforms.com/submissions/v3/integration/submit/21170364/d4fdc4bd-e14d-471c-8057-cd9033781ef8";
+
+    // Example request JSON:
+    var data = {
+      fields: [
+        {
+          name: "email",
+          value: data.email,
+        },
+        {
+          name: "phone",
+          value: data.phone,
+        },
+        {
+          name: "body",
+          value: data.body,
+        },
+        {
+          name: "firstname",
+          value: data.firstname,
+        },
+      ],
+      context: {
+        pageUri: "www.example.com/page",
+        pageName: "Example page",
+      },
+      legalConsentOptions: {
+        // Include this object when GDPR options are enabled
+        consent: {
+          consentToProcess: true,
+          text: "I agree to allow Example Company to store and process my personal data.",
+          communications: [
+            {
+              value: true,
+              subscriptionTypeId: 999,
+              text: "I agree to receive marketing communications from Example Company.",
+            },
+          ],
+        },
+      },
+    };
+
+    var final_data = JSON.stringify(data);
+
+    xhr.open("POST", url);
+    // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        alert(xhr.responseText); // Returns a 200 response if the submission is successful.
+      } else if (xhr.readyState == 4 && xhr.status == 400) {
+        alert(xhr.responseText); // Returns a 400 error the submission is rejected.
+      } else if (xhr.readyState == 4 && xhr.status == 403) {
+        alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.
+      } else if (xhr.readyState == 4 && xhr.status == 404) {
+        alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found
+      }
+    };
+
+    // Sends the request
+
+    xhr.send(final_data);
   };
 
   return (
@@ -45,31 +128,54 @@ const ContactForm = ({ data }) => {
             </div>
           </div>
           <div className="contact-form-form">
-            <form onSubmit={submitFormHandler}>
-              <div className="form-layout">
-                <div className="form-row">
-                  <div className="form-row-unit">
-                    <Input id="name" name="name" placeholder="Your Name" />
+            <FormProvider {...form}>
+              <form onSubmit={handleSubmit(submitFormHandler)}>
+                <div className="form-layout">
+                  <div className="form-row">
+                    <div className="form-row-unit">
+                      <Input
+                        id="firstname"
+                        name="firstname"
+                        placeholder="Your Name"
+                        error={errors?.firstname?.message}
+                      />
+                    </div>
+                    <div className="form-row-unit">
+                      <Input
+                        id="email"
+                        name="email"
+                        placeholder="Your Email"
+                        error={errors?.email?.message}
+                      />
+                    </div>
                   </div>
-                  <div className="form-row-unit">
-                    <Input id="name" name="name" placeholder="Your Name" />
+                  <div className="form-row">
+                    <div className="form-row-unit">
+                      <Input
+                        id="phone"
+                        name="phone"
+                        placeholder="Contact Number"
+                        error={errors?.phone?.message}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-row">
+                    <div className="form-row-unit">
+                      <Input
+                        id="body"
+                        name="body"
+                        placeholder="Your Message"
+                        text
+                        error={errors?.body?.message}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-row">
+                    <Button>Send Message</Button>
                   </div>
                 </div>
-                <div className="form-row">
-                  <div className="form-row-unit">
-                    <Input id="name" name="name" placeholder="Your Name" />
-                  </div>
-                </div>
-                <div className="form-row">
-                  <div className="form-row-unit">
-                    <Input id="name" name="name" placeholder="Your Name" text />
-                  </div>
-                </div>
-                <div className="form-row">
-                  <Button>Send Message</Button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </FormProvider>
           </div>
         </div>
       </Container>
